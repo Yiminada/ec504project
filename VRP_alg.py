511beb7319df3454ca9261e2c6f4172bba66caaa
yanglu149 <112187763+yanglu149@users.noreply.github.com>
2023-05-01 17:35:07 -0400

    Add files via upload

--- VRP_alg.py
+++ VRP_alg.py
@@ -15,8 +15,10 @@ def setupData(filename):
                 client_list.append(vrp.client(lines[0], lines[1], lines[2], lines[3], lines[4], lines[5], lines[6]))
             if(lines[7].isdigit()):
                 vehicle_list.append(vrp.vehicle(lines[7], lines[8]))
+    return vehicle_list, client_list
 def main():
-    setupData("VRP_Data.csv")
+    vehicle_list, client_list = setupData("VRP_Data.csv")
+    
     
 if __name__ == '__main__':
     main()
